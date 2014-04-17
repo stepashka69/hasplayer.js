061b00a7e67385c830146a45c27cd6478545f2b6
Antoine Le Chapelain <alechapelain.ext@orange.com>
2014-04-17 10:56:14 +0200

    Adding flux list for testing

--- debugApp/js/homeCtrl.js
+++ debugApp/js/homeCtrl.js
@@ -24,31 +24,45 @@ angular.module('HAS').controller('homeCtrl', ['$scope', '$location', function($s
 	$scope.data = {};
 
 	$scope.data.fluxList = [{
-		'name': 'Big Buck Bunny',
-		'link': 'http://2is7server1.rd.francetelecom.com/VOD/BBB-SD/big_buck_bunny_1080p_stereo.ism/Manifest',
+		'name': 'VOD 1 - SuperSpeedway',
+		'link': 'http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest',
 		'audioChoice': false,
 		'live': false,
 		'type': 'SMOOTH',
 		'DRM': false
 	}, {
-		'name': 'France 2',
-		'link': 'http://2is7server1.rd.francetelecom.com/C4/C4-46_S2.isml/Manifest',
-		'audioChoice': false,
-		'live': true,
+		'name': 'VOD 2 - Arte',
+		'link': 'http://161.105.176.12/VOD/Arte/C4-51_S1.ism/manifest',
+		'audioChoice': true,
+		'live': false,
 		'type': 'SMOOTH',
 		'DRM': false
 	}, {
-		'name': 'SuperSpeedway',
+		'name': 'VOD 3 - SuperSpeedway DRM',
 		'link': 'http://playready.directtaps.net/smoothstreaming/SSWSS720H264PR/SuperSpeedway_720.ism/Manifest',
 		'audioChoice': false,
 		'live': false,
 		'type': 'SMOOTH',
 		'DRM': true
 	}, {
-		'name': 'Arte',
-		'link': 'http://161.105.176.12/VOD/Arte/C4-51_S1.ism/manifest',
-		'audioChoice': true,
+		'name': 'VOD 4 - DRM',
+		'link': 'http://2is7server1.rd.francetelecom.com/VOD/selection-SD-DRM/selection-SD-DRM.ism/Manifest',
+		'audioChoice': false,
 		'live': false,
+		'type': 'SMOOTH',
+		'DRM': true
+	}, {
+		'name': 'LIVE 1 - France 2',
+		'link': 'http://2is7server1.rd.francetelecom.com/C4/C4-46_S2.isml/Manifest',
+		'audioChoice': false,
+		'live': true,
+		'type': 'SMOOTH',
+		'DRM': false
+	}, {
+		'name': 'LIVE 2 - Arte',
+		'link': 'http://2is7server1.rd.francetelecom.com/C4/C4-49_S1.isml/Manifest',
+		'audioChoice': true,
+		'live': true,
 		'type': 'SMOOTH',
 		'DRM': false
 	}];
