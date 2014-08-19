609c9917e7f19e4a68825bd1f3033472ed60df1d
Antoine Le Chapelain <alechapelain.ext@orange.com>
2014-08-19 17:14:29 +0200

    Gruntfile/Packages.json: cleaning - Added a tool to replace images links in css by base64 images inline

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
   grunt.initConfig({
 
+    path: 'build',
+
+    //Check for syntax errors
     jshint: {
       all: ["app/js/*/**/*.js"],
       options: {
@@ -8,58 +11,96 @@ module.exports = function(grunt) {
       }
     },
 
+    //Copy the index to the build folder
+    copy: {
+      html: {
+        src: 'index.html', dest: '<%= path %>/index.html'
+      }
+    },
+
+    //Configuration for blocks in HTML
     useminPrepare: {
       html: 'index.html',
       options: {
-        dest: 'dist'
+        dest: '<%= path %>'
       }
     },
 
+    //The HTML to parse
     usemin: {
-      html: ['dist/index.html']
+      html: ['<%= path %>/index.html']
     },
 
-    copy: {
-      html: {
-        src: 'index.html', dest: 'dist/index.html'
+    //Get CSS files into one and replace all file url with base64 inline
+    cssUrlEmbed: {
+      encodeDirectly: {
+        files: {
+          '<%= path %>/style.css': [
+          'app/lib/bootstrap/css/bootstrap.min.css',
+          'app/lib/bootstrap/css/bootstrap-glyphicons.css',
+          'app/lib/angular.treeview/css/angular.treeview.css',
+          'app/css/main.css',
+          'app/lib/video/video-4.6.min.css',
+          'app/lib/jquery.ui/jquery-ui-1.10.3.custom.min.css',
+          'app/lib/jquery.ui.labeledSlider/jquery.ui.labeledslider.css'
+          ]
+        },
+        options: {
+          failOnMissingUrl: false
+        }
       }
     },
 
+    //Options for minify CSS
     cssmin: {
       generated: {
         options: {
           keepSpecialComments: 0
         }
       },
-      mini: {
+      style: {
+        options: {
+          keepSpecialComments: 0
+        },
         files: {
-          'dist/style.css': ['dist/style.css']
+          '<%= path %>/style.css': ['<%= path %>/style.css']
         }
       }
     },
 
+    //Write CSS from style.css inline in HTML where block is main
+    htmlbuild: {
+      dist: {
+        src: '<%= path %>/index.html',
+        dest: '<%= path %>',
+        options: {
+          beautify: false,
+          relative: true,
+          styles: {
+            main: ['<%= path %>/style.css']
+          }
+        }
+      }
+    },
+
+    //Options for minify JavaScript
     uglify: {
       generated: {
         options: {
           compress:{
             pure_funcs: [
-            'self.debug.log',   /* set this function « no side effects » so  you can remove it ! */
+            'self.debug.log',
             'this.debug.log',
             'rslt.debug.log'
             ],
             global_defs: {
-              DEBUG: true        /* conditionned code by DEBUG will be remove at build */
+              DEBUG: true
             },
-            drop_console : true,  /* remove console statements */
-            drop_debugger: true,  /* remove debugger statements */
-            warnings: true       /* display compress warnings (lines removal for example) */
+            drop_console : true,
+            drop_debugger: true,
+            warnings: true
           },
-          banner: '/* Last build : @@TIMESTAMPTOREPLACE / git revision : @@REVISIONTOREPLACE */\n' /* add this line at dash.all.js start */
-          // ,
-          // beautify : {        /* to debug purpose : code is more human readable  */
-          //   beautify : true
-          // },
-          // mangle: false       /* to debug purpose : variable names are unchanged */
+          banner: '/* Last build : @@TIMESTAMPTOREPLACE / git revision : @@REVISIONTOREPLACE */\n'
         }
       },
       json: {
@@ -68,66 +109,48 @@ module.exports = function(grunt) {
           mangle: false
         },
         files: {
-          'dist/json.js': ['dist/json.js']
+          '<%= path %>/json.js': ['<%= path %>/json.js']
         }
       } 
     },
 
-    filerev: {
-      options: {
-        encoding: 'utf8',
-        algorithm: 'md5',
-        length: 8
+    //Transform the json files in objects all in one JavaScript file
+    json: {
+      main: {
+        options: {
+          namespace: 'jsonData',
+          includePath: false,
+          processName: function(filename) {
+            return filename.toLowerCase();
+          }
+        },
+        src: ['app/sources.json', 'app/notes.json', 'app/contributors.json', 'app/player_libraries.json', 'app/showcase_libraries.json'],
+        dest: '<%= path %>/json.js'
       }
     },
 
-    uncss: {
-      dist: {
+    //Merge the JavaScript Json file with the index.js one
+    concat: {
+      jsonToIndex: {
+        src: ['<%= path %>/index.js', '<%= path %>/json.js'],
+        dest: '<%= path %>/index.js',
+      },
+    },
+
+    //Minify the HTML
+    htmlmin: {
+      main: {
         options: {
-          ignore: [
-          /(#|\.)fancybox(\-[a-zA-Z]+)?/,
-          ".open>.dropdown-menu",
-          ".nav>li",
-          ".nav-tabs>li",
-          ".nav-tabs>li.active>a",
-          ".nav-tabs>li.active>a:hover",
-          ".nav-tabs>li.active>a:focus",
-          ".tab-content>.active",
-          ".pill-content>.active",
-          ".tab-content>.tab-pane", 
-          ".pill-content>.pill-pane",
-          ".fade",
-          ".fade.in",
-          ".collapse",
-          ".collapse.in",
-          ".navbar-collapse",
-          ".navbar-collapse.in",
-          ".collapsing",
-          ".alert-danger",
-          ".visible-xs",
-          ".noscript-warning"
-          ],
+          removeComments: true,
+          collapseWhitespace: true
         },
         files: {
-          'dist/style.css': ['dist/index.html']
+          '<%= path %>/index.html': '<%= path %>/index.html'
         }
       }
     },
 
-    htmlbuild: {
-      dist: {
-        src: 'dist/index.html',
-        dest: 'dist/',
-        options: {
-          beautify: false,
-          relative: true,
-          styles: {
-            main: ['dist/style.css']
-          }
-        }
-      }
-    },
-
+    //Get the revision info from git
     revision: {
       options: {
         property: 'meta.revision',
@@ -136,6 +159,7 @@ module.exports = function(grunt) {
       }
     },
 
+    //Put the revision info in JS files
     replace: {
       all: {
         options: {
@@ -151,96 +175,60 @@ module.exports = function(grunt) {
           ]
         },
         files: [
-        {expand: true, flatten: true, src: ['dist/player.js', 'dist/index.js'], dest: 'dist'}
+        {expand: true, flatten: true, src: ['<%= path %>/player.js', '<%= path %>/index.js'], dest: '<%= path %>'}
         ]
       }
     },
 
-    json: {
-      main: {
-        options: {
-          namespace: 'jsonData',
-          includePath: false,
-          processName: function(filename) {
-            return filename.toLowerCase();
-          }
-        },
-        src: ['app/sources.json', 'app/notes.json', 'app/contributors.json', 'app/player_libraries.json', 'app/showcase_libraries.json'],
-        dest: 'dist/json.js'
-      }
-    },
-
-    concat: {
-      jsonToIndex: {
-        src: ['dist/index.js', 'dist/json.js'],
-        dest: 'dist/index.js',
-      },
-    },
-
-    htmlmin: {
-      main: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
-        },
-        files: {
-          'dist/index.html': 'dist/index.html'
-        }
-      }
-    },
-
+    //Remove folder at start and temporary files in the end
     clean: {
       start: {
-        src: ['dist']
+        src: ['<%= path %>']
       },
       end: {
-        src: ['dist/style.css', 'dist/json.js']
+        src: ['<%= path %>/style.css', '<%= path %>/json.js']
       }
     }
-
 
   });
 
   // Require needed grunt-modules
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-git-revision');
+  grunt.loadNpmTasks('grunt-usemin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-filerev');
-  grunt.loadNpmTasks('grunt-usemin');
-  // grunt.loadNpmTasks('grunt-uncss');
-  grunt.loadNpmTasks('grunt-html-build');
-  grunt.loadNpmTasks('grunt-git-revision');
-  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-css-url-embed');
   grunt.loadNpmTasks('grunt-json');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-html-build');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-replace');
 
 
   grunt.registerTask('test', [
-    'jshint',
-  ]);
+    'jshint'
+    ]);
 
-  // Define tasks
   grunt.registerTask('build', [
-    'clean:start',
-    'revision',
-    'copy',
-    'useminPrepare',
-    'concat:generated',
-    'cssmin:generated',
-    'uglify:generated',
-    'filerev',
-    'usemin',
-    // 'uncss', //Causing troubles with graphics elements not loading at start
-    'cssmin:mini',
-    'htmlbuild',
-    'replace',
-    'json',
-    'uglify:json',
-    'concat:jsonToIndex',
-    'htmlmin:main',
-    'clean:end'
+    'clean:start',        //empty folder
+    'copy',               //copy HTML file
+    'revision',           //get git info
+    'useminPrepare',      //get files in blocks tags
+    'concat:generated',   //merge all the files in one for each blocks
+    'cssUrlEmbed',        //get the CSS files and merge into one
+    'cssmin:style',       //minify the generated CSS file
+    'cssmin:generated',   //minify the CSS in blocks (none)
+    'uglify:generated',   //minify the JS in blocks
+    'json',               //get the json files into a json.js
+    'uglify:json',        //minify the json.js file
+    'concat:jsonToIndex', //merge the json.js file with index.js
+    'usemin',             //replace the tags blocks by the result
+    'htmlbuild',          //inline the CSS
+    'htmlmin:main',       //Minify the HTML
+    'replace',            //Add the git info in files
+    'clean:end'           //Clean temp files
     ]);
 };
