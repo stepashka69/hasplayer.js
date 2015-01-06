c672c5de62b8a009818eb15929d55a8e9e746973
nyga7491 <nicolas.angot@orange.com>
2015-01-06 15:26:48 +0100

    update ip selenium server

--- testIntern/intern.js
+++ testIntern/intern.js
@@ -6,10 +6,10 @@ define({
 	proxyPort: 3555,
 
 	// A fully qualified URL to the Intern proxy
-	proxyUrl: 'http://10.194.60.158:3555',
+	proxyUrl: 'http://10.194.60.93:3555',
 	tunnel: 'NullTunnel',
 	tunnelOptions: {
-        hostname: '10.194.60.158',
+        hostname: '10.194.60.93',
         port: '4444',
         verbose: true
     },
