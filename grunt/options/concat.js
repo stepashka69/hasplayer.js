9883030aa90a9189266013184b95dff1ac831ddc
Jérémie Ledentu <jledentu@gmail.com>
2015-02-17 12:03:13 +0100

    Remove metricsAgent grunt tasks

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -4,17 +4,4 @@ module.exports = {
 		src: ['<%= path %>/dashif.js', '<%= path %>/json.js'],
 		dest: '<%= path %>/dashif.js',
 	},
-
-	agent: {
-		src: [
-		'<%= metricsAgent %>/MetricsAgent.js', 
-		'<%= metricsAgent %>/MetricsDatabase.js',
-		'<%= metricsAgent %>/MetricsSender.js',
-		'<%= metricsAgent %>/collectors/HasPlayerCollector.js',
-		'<%= metricsAgent %>/formatters/CSQoEFormatter.js',
-		'<%= metricsAgent %>/MetricsVo.js'
-		],
-		dest: '<%= path %>/metricsAgent.js',
-	}
-	
 };
