361f25971bf5506b5692219d9d5f6942ea4cd58d
Nicolas ANGOT <nicolas.angot@orange.com>
2015-04-03 17:42:52 +0200

    add a button to reset all metrics.

--- server/metricsViewer/js/metricsController.js
+++ server/metricsViewer/js/metricsController.js
@@ -10,4 +10,18 @@ angular.module('metricsViewer', ['luegg.directives']).controller('metricsControl
 	$scope.contentDuration = null;
 	$scope.startupTime = null;
 	$scope.firstMetricTime = null;
+
+	$scope.resetMetrics = function() {
+		$scope.playerId = null;
+		$scope.sessionId = null;
+		$scope.url = null;
+		$scope.fullScreen = false;
+		$scope.mediaType = null;
+		$scope.metrics = [];
+		$scope.encodingFormat = null;
+		$scope.encapsulation = null;
+		$scope.contentDuration = null;
+		$scope.startupTime = null;
+		$scope.firstMetricTime = null;
+  	};
 });
