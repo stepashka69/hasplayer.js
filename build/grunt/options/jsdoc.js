99b4750a2d2df3b1988269d09e6ae3474274b93b
Nicolas ANGOT <nicolas.angot@orange.com>
2015-05-29 15:23:04 +0200

    update doc generation

--- build/grunt/options/jsdoc.js
+++ build/grunt/options/jsdoc.js
@@ -2,9 +2,9 @@ module.exports = {
 	dist : {
 		src: ['../app/js/streaming/MediaPlayer.js'],
 		options:{
-			destination: '../doc/JSDoc/MediaPlayer/',
+			destination: '<%= path %>/doc/JSDoc/MediaPlayer/',
 			configure: '../doc/JSDoc/Template/conf.json',
-			template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template'
+			template: './node_modules/grunt-jsdoc/node_modules/ink-docstrap/template'
 		}
 	}
 };
