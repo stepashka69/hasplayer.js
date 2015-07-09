c9fb40669bbb324c9654b3558f84ffa615613b9e
Nicolas ANGOT <nicolas.angot@orange.com>
2015-07-09 15:33:47 +0200

    update web app

--- samples/OrangeHasplayerDemo/js/main.js
+++ samples/OrangeHasplayerDemo/js/main.js
@@ -1,4 +1,13 @@
-var playerLoader = null;
+var orangeHasPlayer = null,
+    video = null,
+    volumeButton = null,
+    fullscreenButton = null,
+    audioList = null,
+    subtitleList = null,
+    audioTracks = [],
+    currentaudioTrack = null,
+    subtitleTracks = [],
+    currentsubtitleTrack = null;
 
 window.onload = function() {
     var xhr = new XMLHttpRequest();
@@ -6,21 +15,24 @@ window.onload = function() {
     xhr.open('GET', document.location + '/../json/sources.json');
     xhr.onreadystatechange = function() {
         //if (xhr.readyState === 3) {
-        if (xhr.readyState==4 && xhr.status==200) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
             buildStreamsList(xhr.responseText);
         }
     }
     xhr.send();
 
-    playerLoader = new PlayerLoader();
+    getDOMElements();
+    createHasPlayer();
+    registerHasPlayerEvents();
+    registerGUIEvents();
 
-    var buildStreamsList = function (jsonList) {
+    var buildStreamsList = function(jsonList) {
         // Prepare stream table
         var tableNode = document.getElementById('streams-table'),
             i;
 
         if (tableNode) {
-            while(tableNode.firstChild) {
+            while (tableNode.firstChild) {
                 tableNode.removeChild(tableNode.firstChild);
             }
         } else {
@@ -44,11 +56,11 @@ window.onload = function() {
 
     var createStreamEntry = function(stream) {
         var streamItem = document.createElement('tr'),
-        streamItemName = document.createElement('td'),
-        streamItemProtocol = document.createElement('td'),
-        streamItemType = document.createElement('td'),
-        streamItemTypeIcon = document.createElement('img'),
-        streamItemProtection = document.createElement('td');
+            streamItemName = document.createElement('td'),
+            streamItemProtocol = document.createElement('td'),
+            streamItemType = document.createElement('td'),
+            streamItemTypeIcon = document.createElement('img'),
+            streamItemProtection = document.createElement('td');
 
         streamItem.appendChild(streamItemType);
         streamItem.appendChild(streamItemName);
@@ -67,7 +79,7 @@ window.onload = function() {
 
         var protections = [];
         if (stream.protData) {
-            var protectionsNames  = Object.getOwnPropertyNames(stream.protData);
+            var protectionsNames = Object.getOwnPropertyNames(stream.protData);
             for (var i = 0, len = protectionsNames.length; i < len; i++) {
                 if (S(protectionsNames[i]).contains('playready')) {
                     protections.push("PR");
@@ -82,7 +94,8 @@ window.onload = function() {
         streamItem.setAttribute('class', 'stream-item');
 
         var onStreamClicked = function(streamInfos) {
-            playerLoader.loadStream(streamInfos);
+            reset();
+            loadStream(streamInfos);
         }
 
         streamItem.addEventListener('click', function() {
@@ -93,10 +106,131 @@ window.onload = function() {
     }
 }
 
+var getDOMElements = function() {
+    video = document.getElementById('player');
+    volumeButton = document.getElementById('button-volume');
+    fullscreenButton = document.getElementById('button-fullscreen');
+    audioList = document.getElementById('audioCombo');
+    subtitleList = document.getElementById('subtitleCombo');
+}
+
+var registerGUIEvents = function() {
+    volumeButton.addEventListener('click', onMuteClicked)
+    fullscreenButton.addEventListener('click', onFullScreenClicked);
+}
+
 var audioChanged = function() {
-    playerLoader.changeAudio();
+    changeAudio();
 }
 
 var subtitleChanged = function() {
-    playerLoader.changeSubtitle();
+    changeSubtitle();
+}
+
+var handleAudioDatas = function(_audioTracks, _selectedAudioTrack){
+    audioTracks = _audioTracks;
+    currentaudioTrack = _selectedAudioTrack;
+
+    addCombo(audioTracks, audioList);
+    selectCombo(audioTracks, audioList, currentaudioTrack);
+}
+
+var handleSubtitleDatas = function(_subtitleTracks, _selectedSubtitleTrack){
+    //init subtitles tracks
+    subtitleTracks = _subtitleTracks;
+    currentsubtitleTrack = _selectedSubtitleTrack;
+
+    addCombo(subtitleTracks, subtitleList);
+    selectCombo(subtitleTracks, subtitleList, currentsubtitleTrack);
+}
+
+var handleError = function(e){
+
+}
+
+var addCombo = function(tracks, combo) {
+    var i, option;
+
+    for (i = 0; i < tracks.length; i++) {
+        option = document.createElement("option");
+        option.text = tracks[i].id;
+        option.value = tracks[i].lang;
+
+        try {
+            combo.add(option, null); //Standard 
+        } catch (error) {
+            combo.add(option); // IE only
+        }
+        if (combo.style.visibility === 'hidden') {
+            combo.style.visibility = 'visible';
+        }
+    }
+}
+
+var selectCombo = function(tracks, combo, currentTrack) {
+    var i;
+
+    for (i = 0; i < tracks.length; i++) {
+        if (currentTrack === tracks[i]) {
+            combo.selectedIndex = i;
+        }
+    }
+}
+
+var resetCombo = function(tracks, combo) {
+    var i;
+
+    for (i = tracks.length - 1; i >= 0; i--) {
+        combo.options.remove(i);
+    }
+
+    tracks = [];
+
+    combo.style.visibility = 'hidden';
+}
+
+var reset = function() {
+    resetCombo(audioTracks, audioList);
+    resetCombo(subtitleTracks, subtitleList);
+
+    currentaudioTrack = null;
+    currentsubtitleTrack = null;
+}
+
+var onMuteClicked = function() {
+    setPlayerMute();
+    setVolumeOff(orangeHasPlayer.getMute());
+}
+
+var onFullScreenClicked = function() {
+    if (!document.fullscreenElement && // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) { // current working methods
+        if (document.getElementById("playerVideo-container").requestFullscreen) {
+            document.getElementById("playerVideo-container").requestFullscreen();
+        } else if (document.getElementById("playerVideo-container").msRequestFullscreen) {
+            document.getElementById("playerVideo-container").msRequestFullscreen();
+        } else if (document.getElementById("playerVideo-container").mozRequestFullScreen) {
+            document.getElementById("playerVideo-container").mozRequestFullScreen();
+        } else if (document.getElementById("playerVideo-container").webkitRequestFullscreen) {
+            document.getElementById("playerVideo-container").webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+}
+
+var setVolumeOff = function(value) {
+    if (value) {
+        volumeButton.className = "fa fa-volume-off button button-volume left";
+    } else {
+        volumeButton.className = "fa fa-volume-up button button-volume left";
+    }
 }
--- samples/OrangeHasplayerDemo/js/orangeHasPlayer.js
+++ samples/OrangeHasplayerDemo/js/orangeHasPlayer.js
@@ -1,52 +1,79 @@
-var PlayerLoader = function() {
-    var orangeHasPlayer,
-    video = document.getElementById('player'),
-    audioList = document.getElementById('audioCombo'),
-    subtitleList = document.getElementById('subtitleCombo'),
-    previousPlayedQuality = 0,
-    previousDownloadedQuality = 0,
-    config,
-    configMetrics = {
-        "name": "csQoE (local)",
-        "activationUrl": "http://localhost:8080/config",
-        "serverUrl": "http://localhost:8080/metrics",
-        "dbServerUrl": "http://localhost:8080/metricsDB",
-        "collector": "HasPlayerCollector",
-        "formatter": "CSQoE",
-        "sendingTime": 10000
-    },
-    audioTracks = [],
-    currentaudioTrack = null,
-    subtitleTracks = [],
-    currentsubtitleTrack = null,
+    var previousPlayedQuality = 0,
+        previousDownloadedQuality = 0,
+        config,
+        configMetrics = {
+            "name": "csQoE (local)",
+            "activationUrl": "http://localhost:8080/config",
+            "serverUrl": "http://localhost:8080/metrics",
+            "dbServerUrl": "http://localhost:8080/metricsDB",
+            "collector": "HasPlayerCollector",
+            "formatter": "CSQoE",
+            "sendingTime": 10000
+        };
 
-    orangeHasPlayer = new OrangeHasPlayer();
-    orangeHasPlayer.init(video);
+    function createHasPlayer() {
+        orangeHasPlayer = new OrangeHasPlayer();
+        orangeHasPlayer.init(video);
+        loadHasPlayerConfig('hasplayer_config.json');
+        orangeHasPlayer.loadMetricsAgent(configMetrics);
 
-    loadHasPlayerConfig('hasplayer_config.json');
-    orangeHasPlayer.loadMetricsAgent(configMetrics);
+        orangeHasPlayer.setDefaultAudioLang('deu');
+        orangeHasPlayer.setDefaultSubtitleLang('fre');
+    };
 
-    orangeHasPlayer.setDefaultAudioLang('deu');
-    orangeHasPlayer.setDefaultSubtitleLang('fre');
+    function registerHasPlayerEvents() {
+        orangeHasPlayer.addEventListener("error", onError);
+        orangeHasPlayer.addEventListener("subtitlesStyleChanged", onSubtitlesStyleChanged);
+        orangeHasPlayer.addEventListener("loadeddata", onload);
+        orangeHasPlayer.addEventListener("play_bitrate", onPlayBitrateChanged);
+        orangeHasPlayer.addEventListener("download_bitrate", onDownloadBitrateChanged);
+        orangeHasPlayer.addEventListener("volumechange", onVolumeChange);
+    };
 
-    orangeHasPlayer.addEventListener("loadeddata", onload.bind(this));
-
-    function onload(e) {
+    /********************************************************************************************************************
+    *
+    *
+    *                   OrangeHasPlayer Events
+    *
+    *
+    **********************************************************************************************************************/
+    function onload() {
+        //handle onload events to get audio, subtitles tracks
         //init audio tracks
-        audioTracks = orangeHasPlayer.getAudioTracks();
-        currentaudioTrack = orangeHasPlayer.getSelectedAudioTrack();
+        handleAudioDatas(orangeHasPlayer.getAudioTracks(), orangeHasPlayer.getSelectedAudioTrack());
+        //init subtitle tracks
+        handleSubtitleDatas(orangeHasPlayer.getSubtitleTracks(), orangeHasPlayer.getSelectedSubtitleTrack());
+    };
 
-        addCombo(audioTracks, audioList);
-        selectCombo(audioTracks, audioList, currentaudioTrack);
+    function onVolumeChange(){
 
-        //init subtitles tracks
-        subtitleTracks = orangeHasPlayer.getSubtitleTracks();
-        currentsubtitleTrack = orangeHasPlayer.getSelectedSubtitleTrack();
+    };
 
-        addCombo(subtitleTracks, subtitleList);
-        selectCombo(subtitleTracks, subtitleList, currentsubtitleTrack);
-    }
+    function onFullScreenChange(){
 
+    };
+
+    function onSubtitlesStyleChanged() {
+
+    };
+
+    function onError(e) {
+        handleError(e);
+    };
+
+    function onPlayBitrateChanged(){
+
+    };
+
+    function onDownloadBitrateChanged() {
+
+    };
+
+    function onVolumeChange() {
+
+    };
+    /***************************************************************************************************************************/
+    
     function loadHasPlayerConfig(fileUrl) {
         var reqConfig = new XMLHttpRequest();
 
@@ -64,67 +91,18 @@ var PlayerLoader = function() {
         reqConfig.send();
     };
 
-    function addCombo(tracks, combo) {
-        var i, option;
-
-        for (i = 0; i < tracks.length; i++) {
-            option = document.createElement("option");
-            option.text = tracks[i].id;
-            option.value = tracks[i].lang;
-
-            try {
-                combo.add(option, null); //Standard 
-            }catch(error) {
-                combo.add(option); // IE only
-            }
-            if (combo.style.visibility === 'hidden') {
-                combo.style.visibility = 'visible';
-            }
-        }
-    }
-
-    function selectCombo(tracks, combo, currentTrack) {
-        var i;
-
-        for (i = 0; i < tracks.length; i++) {
-            if (currentTrack === tracks[i]) {
-                combo.selectedIndex = i;
-            }
-        }
-    }
-
-    function resetCombo(tracks, combo) {
-        var i;
-
-        for (i = tracks.length - 1; i >= 0 ; i--) {
-            combo.options.remove(i);
-        }
-
-        tracks = [];
-
-        combo.style.visibility='hidden';
-    }
-
-    function reset() {
-        resetCombo(audioTracks, audioList);
-        resetCombo(subtitleTracks, subtitleList);
-
-        currentaudioTrack = null;
-        currentsubtitleTrack = null;
-    }
-
-    return {
-        loadStream: function(streamInfos) {
-            reset();
-            orangeHasPlayer.load(streamInfos.url, streamInfos.protData);
-        },
-
-        changeAudio: function() {            
-            orangeHasPlayer.setAudioTrack(audioTracks[audioList.selectedIndex]);
-        },
-
-        changeSubtitle: function() {            
-            orangeHasPlayer.setSubtitleTrack(subtitleTracks[subtitleList.selectedIndex]);
-        }
+    function loadStream(streamInfos) {
+        orangeHasPlayer.load(streamInfos.url, streamInfos.protData);
     };
-}
+
+    function changeAudio() {
+        orangeHasPlayer.setAudioTrack(audioTracks[audioList.selectedIndex]);
+    };
+
+    function changeSubtitle() {
+        orangeHasPlayer.setSubtitleTrack(subtitleTracks[subtitleList.selectedIndex]);
+    };
+
+    function setPlayerMute() {
+        orangeHasPlayer.setMute(!orangeHasPlayer.getMute());
+    };
