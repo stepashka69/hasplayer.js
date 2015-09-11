ff8e536981d8c7b5e2125dd052fafa43b8662007
Matthieu Hazon <mhazon.ext@orange.com>
2015-09-11 17:25:50 +0200

    Move utility functions to dedicated module.

--- samples/OrangeHasplayerDemo/js/main.js
+++ samples/OrangeHasplayerDemo/js/main.js
@@ -808,32 +808,3 @@ var enableMiddleContainer = function(enabled) {
         closeButton.className = 'op-close op-hidden';
     }
 };
-
-var setTimeWithSeconds = function(sec) {
-    var sec_num = parseInt(sec, 10); // don't forget the second param
-    var hours = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
-
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
-    if (seconds < 10) {
-        seconds = '0' + seconds;
-    }
-    var time = hours + ':' + minutes + ':' + seconds;
-    return time;
-};
-
-var hasClass = function(element, className) {
-    return element.className && new RegExp('(^|\\s)' + className + '(\\s|$)').test(element.className);
-};
-
-var clearContent = function(element) {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }
-};
