287164dee19e745046e38fd9609ba6d6a0b5e3ec
Matthieu Hazon <mhazon.ext@orange.com>
2015-09-11 15:23:26 +0200

    Refactoring of Orange Has Player streams list.

--- samples/OrangeHasplayerDemo/js/main.js
+++ samples/OrangeHasplayerDemo/js/main.js
@@ -21,7 +21,7 @@ var video = null,
     seekbarContainer = null,
     seekbar = null,
     seekbarBackground = null,
-    videoDuration = null;
+    videoDuration = null,
     durationTimeSpan = null,
     elapsedTimeSpan = null,
 
@@ -48,16 +48,6 @@ var video = null,
 
     // Spinner
     loadingElement = null,
-
-    // Streams panel
-    selectedStreamElement = null,
-    streamFilters = {
-        vod: true,
-        live: true,
-        hls: true,
-        mss: true,
-        dash: true
-    },
 
     // Quick settings
     audioListCombobox = null,
@@ -119,143 +109,12 @@ var video = null,
     };
 
 window.onload = function() {
-
-    var initStreamTable = function() {
-         // Prepare stream table
-        var tableNode = document.getElementById('streams-table');
-
-        if (tableNode) {
-            clearContent(tableNode);
-        } else {
-            tableNode = document.createElement('table');
-            tableNode.id = 'streams-table';
-            document.getElementById('streams-container').appendChild(tableNode);
-        }
-
-        return tableNode;
-    };
-
-    var buildStreamsList = function(jsonList) {
-        var tableNode = initStreamTable(),
-            streamsList = JSON.parse(jsonList);
-
-        // Add stream elements
-        for (var i = 0, len = streamsList.items.length; i < len; i++) {
-            var stream = streamsList.items[i];
-
-            if (stream.protocol) {
-                var streamItem = createStreamEntry(stream);
-                tableNode.appendChild(streamItem);
-            }
-        }
-    };
-
-    var createStreamEntry = function(stream) {
-        var streamItem = document.createElement('tr'),
-            streamItemName = document.createElement('td'),
-            streamItemProtocol = document.createElement('td'),
-            streamItemType = document.createElement('td'),
-            streamItemTypeIcon = document.createElement('img'),
-            streamItemProtection = document.createElement('td'),
-            className = 'stream-item';
-
-        streamItem.appendChild(streamItemType);
-        streamItem.appendChild(streamItemName);
-        streamItem.appendChild(streamItemProtocol);
-        streamItem.appendChild(streamItemProtection);
-
-        if (stream.type.toLowerCase() === 'live') {
-            className += ' stream-live';
-            streamItemTypeIcon.src = 'res/live_icon.png';
-        } else if (stream.type.toLowerCase() === 'vod') {
-            className += ' stream-vod';
-            streamItemTypeIcon.src = 'res/vod_icon.png';
-        }
-
-        streamItemType.appendChild(streamItemTypeIcon);
-        streamItemName.innerHTML = stream.name;
-        streamItemProtocol.innerHTML = stream.protocol;
-        className += ' stream-' + stream.protocol.toLowerCase();
-
-        var protections = [];
-        if (stream.protData) {
-            var protectionsNames = Object.getOwnPropertyNames(stream.protData);
-            for (var i = 0, len = protectionsNames.length; i < len; i++) {
-                if (S(protectionsNames[i]).contains('playready')) {
-                    className += ' stream-playready';
-                    protections.push('PR');
-                } else if (S(protectionsNames[i]).contains('widevine')) {
-                    className += ' stream-widevine';
-                    protections.push('WV');
-                }
-            }
-        }
-
-        streamItemProtection.innerHTML = protections.join(',');
-
-        streamItem.setAttribute('class', className);
-
-        streamItem.addEventListener('click', function() {
-            if (selectedStreamElement !== null) {
-                selectedStreamElement.id = '';
-            }
-
-            selectedStreamElement = this;
-            selectedStreamElement.id = 'stream-selected';
-            onStreamClicked(stream);
-        });
-
-        return streamItem;
-    };
-
-    var loadStreamList = function() {
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', document.location + '/../json/sources.json');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                buildStreamsList(xhr.responseText);
-            }
-        };
-        xhr.send();
-    };
-
-    loadStreamList();
+    var streamsPanel = new StreamsPanel();
+    streamsPanel.init();
     initMetricsAgentOptions();
     getDOMElements();
     createHasPlayer();
     registerGUIEvents();
-};
-
-var initStreamListFilter = function() {
-    var vodFilter = document.getElementById('display-vod-streams'),
-    liveFilter = document.getElementById('display-live-streams'),
-    hlsFilter = document.getElementById('display-hls-streams'),
-    mssFilter = document.getElementById('display-mss-streams'),
-    dashFilter = document.getElementById('display-dash-streams');
-
-    var filterStreams = function() {
-        var elts = document.getElementsByClassName('stream-item');
-
-        for (var i = 0, len = elts.length; i < len; i++) {
-            var hidden = false;
-            if ((streamFilters.vod && hasClass(elts[i], 'stream-vod') ||
-                streamFilters.live && hasClass(elts[i], 'stream-live')) &&
-                (streamFilters.hls && hasClass(elts[i], 'stream-hls') ||
-                streamFilters.mss && hasClass(elts[i], 'stream-mss') ||
-                streamFilters.dash && hasClass(elts[i], 'stream-dash'))) {
-                elts[i].style.display = '';
-            } else {
-                elts[i].style.display = 'none';
-            }
-        }
-    };
-
-    vodFilter.addEventListener('click', function(e) { streamFilters.vod = this.checked; filterStreams(); });
-    liveFilter.addEventListener('click', function(e) { streamFilters.live = this.checked; filterStreams(); });
-    hlsFilter.addEventListener('click', function(e) { streamFilters.hls = this.checked; filterStreams(); });
-    mssFilter.addEventListener('click', function(e) { streamFilters.mss = this.checked; filterStreams(); });
-    dashFilter.addEventListener('click', function(e) { streamFilters.dash = this.checked; filterStreams(); });
 };
 
 var initMetricsAgentOptions = function() {
@@ -370,7 +229,7 @@ var registerGUIEvents = function() {
     seekbarBackground.addEventListener('click', onSeekClicked);
     seekbar.addEventListener('click', onSeekClicked);
 
-    settingsMenuButton.addEventListener('click', onGlobalMenuClicked);
+    settingsMenuButton.addEventListener('click', onSettingsMenuButtonClicked);
 
     enableMetricsCheckbox.addEventListener('click', onEnableMetrics);
     metricsAgentCombobox.addEventListener('change', onSelectMetricsAgent);
@@ -380,7 +239,7 @@ var registerGUIEvents = function() {
 
     enableOptimzedZappingCheckbox.addEventListener('click', onEnableOptimizedZapping);
 
-    initStreamListFilter();
+
 };
 
 /********************************************************************************************************************
@@ -606,7 +465,7 @@ var onCloseButtonClicked = function() {
     showControlBar();
 };
 
-var onGlobalMenuClicked = function() {
+var onSettingsMenuButtonClicked = function() {
     if (hasClass(menuContainer, 'hidden')) {
         menuContainer.className = '';
     } else {
@@ -655,7 +514,6 @@ var onEnableOptimizedZapping = function(e) {
  **********************************************************************************************************************/
 
 var createLanguageLine = function(audioTrack, selectedAudioTrack, type) {
-
     var checked = selectedAudioTrack.id === audioTrack.id ? 'checked="checked"' : '';
     var lang =  audioTrack.lang !== undefined ? audioTrack.lang : audioTrack.id;
     var html = '<div class="op-languages-line">' +
@@ -1122,12 +980,12 @@ var setTimeWithSeconds = function(sec) {
     return time;
 };
 
-function hasClass(element, className) {
+var hasClass = function(element, className) {
     return element.className && new RegExp('(^|\\s)' + className + '(\\s|$)').test(element.className);
 };
 
-function clearContent(element) {
+var clearContent = function(element) {
     while (element.firstChild) {
         element.removeChild(element.firstChild);
     }
-}
+};
