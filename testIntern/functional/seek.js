f1cf0674e9ed9af33df23e748b24b0f9eff63404
Matthieu Hazon <mhazon.ext@orange.com>
2015-09-24 17:47:37 +0200

    Add seek tests of seek repeating.

--- testIntern/functional/seek.js
+++ testIntern/functional/seek.js
@@ -102,13 +102,90 @@ define([
             });
         };
 
-        var i, j;
+        var test_repeat_seek_nodelay = function(seekTime) {
+
+            registerSuite({
+                name: 'Test seeking functionnality',
+
+                'Do seek with no delay': function() {
+
+                    console.log('[TEST_SEEK] Seek to ' + seekTime + 's...');
+
+                    return command.execute(seek, [seekTime])
+                    .then(pollUntil(
+                        function (seekTime) {
+                            var time = document.querySelector('video').currentTime;
+                            return (time > seekTime) ? true : null;
+                        }, [seekTime], 10000))
+                    .then(function () {
+                        return command.execute(getVideoCurrentTime)
+                        .then(function (time) {
+                            console.log('[TEST_SEEK] current time = ' + time);
+                            assert.ok((time - seekTime) < 0.5);
+                            videoCurrentTime = time;
+                        });
+                    }, function (error) {
+                        assert.ok(false, '[TEST_SEEK] Failed to seek');
+                    });
+                }
+            });
+        };
+
+        var test_repeat_seek_delay = function(seekTime) {
+
+            registerSuite({
+                name: 'Test seeking functionnality',
+
+                'Do seek with no delay': function() {
+
+                    console.log('[TEST_SEEK] Seek to ' + seekTime + 's...');
+
+                    return command.execute(seek, [seekTime])
+                    .then(pollUntil(
+                        function (seekTime) {
+                            var time = document.querySelector('video').currentTime;
+                            return (time > seekTime) ? true : null;
+                        }, [seekTime], 10000))
+                    .then(function () {
+                        return command.execute(getVideoCurrentTime)
+                        .then(function (time) {
+                            console.log('[TEST_SEEK] current time = ' + time);
+                            assert.ok((time - seekTime) < 0.5);
+                            videoCurrentTime = time;
+                            return true;
+                        });
+                    }, function (error) {
+                        assert.ok(false, '[TEST_SEEK] Failed to seek');
+                    }).sleep(2000).execute(getVideoCurrentTime)
+                    .then(function (time) {
+                        console.log('[TEST_SEEK] current time = ' + time);
+                        assert.ok((time - videoCurrentTime) > 1.5, 'Test if video plays after seek');
+                        videoCurrentTime = time;
+                        return true;
+                    });
+                }
+            });
+        };
+
+        var i, j,
+            randomSeekPosition = 0;
 
         for (i = 0; i < config.seek.length; i++) {
             test_init(config.seek[i].stream);
             for (j = 0; j < config.seek[i].seekCount; j++) {
                 // Generate a random seek time
-                test_seek(Math.round(Math.random() * config.seek[i].duration * 100) / 100);
+                randomSeekPosition = Math.round(Math.random() * config.seek[i].duration * 100) / 100;
+                test_seek(randomSeekPosition);
+            }
+
+            randomSeekPosition = Math.round(Math.random() * config.seek[i].duration * 100) / 100;
+            for (j = 0; j < config.seek[i].seekCount; j++) {
+                test_repeat_seek_nodelay(randomSeekPosition);
+            }
+
+            randomSeekPosition = Math.round(Math.random() * config.seek[i].duration * 100) / 100;
+            for (j = 0; j < config.seek[i].seekCount; j++) {
+                test_repeat_seek_delay(randomSeekPosition);
             }
         }
 });
