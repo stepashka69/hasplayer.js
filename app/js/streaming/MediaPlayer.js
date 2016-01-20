15ea26a802bdc35ea97886f40121edd6de997425
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-01-20 15:43:08 +0100

    v1.2.6.1

--- app/js/streaming/MediaPlayer.js
+++ app/js/streaming/MediaPlayer.js
@@ -49,11 +49,7 @@ MediaPlayer = function(aContext) {
      *
      */
     var VERSION = "1.2.0",
-<<<<<<< HEAD
-        VERSION_HAS = "1.2.6",
-=======
-        VERSION_HAS = "1.2.7_dev",
->>>>>>> origin/development
+        VERSION_HAS = "1.2.6.1",
         GIT_TAG = "@@REVISION",
         BUILD_DATE = "@@TIMESTAMP",
         context = aContext,
