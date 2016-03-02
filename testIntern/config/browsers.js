131a131e7449e72cab6af864704975849408e659
mbrechet <mbrechet.ext@orange.com>
2016-03-02 17:46:00 +0100

    [TEST_INTERN] add chromeOptions to reference user profile and enable component update

--- testIntern/config/browsers.js
+++ testIntern/config/browsers.js
@@ -1,7 +1,12 @@
 
 define(function () {
 
-    var CHROME_CONFIG  = { browserName: 'chrome', /*version: '2.11', */platform: 'WINDOWS' };
+    var CHROME_CONFIG  = { browserName: 'chrome', /*version: '2.11', */platform: 'WINDOWS',
+                             "chromeOptions":{
+                                    "args": ["user-data-dir=profiles/chrome/User Data"],
+                                    "excludeSwitches":["disable-component-update"]
+                             }              
+                         };
     var IE11_CONFIG    = { browserName: 'internet explorer', /*version: '11', */platform: 'WINDOWS' };
     var EDGE_CONFIG    = { browserName: 'MicrosoftEdge', platform: 'WINDOWS' };
 
