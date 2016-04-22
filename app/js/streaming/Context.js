4f1153d16d46be5669b3139925fdf00bdab68ae2
Nicolas ANGOT <nicolas.angot@orange.com>
2016-04-22 15:56:33 +0200

    respect alphabetical order for inclusion of JavaScript files

--- app/js/streaming/Context.js
+++ app/js/streaming/Context.js
@@ -59,11 +59,11 @@ MediaPlayer.di.Context = function () {
             // MediaPlayer.dependencies.*
             this.system.mapSingleton('abrController', MediaPlayer.dependencies.AbrController);
             this.system.mapClass('bufferController', MediaPlayer.dependencies.BufferController);
-            this.system.mapClass('fragmentInfoController', MediaPlayer.dependencies.FragmentInfoController);
             this.system.mapSingleton('bufferExt', MediaPlayer.dependencies.BufferExtensions);
             this.system.mapSingleton('errHandler', MediaPlayer.dependencies.ErrorHandler);
             this.system.mapClass('eventController', MediaPlayer.dependencies.EventController);
             this.system.mapClass('fragmentController', MediaPlayer.dependencies.FragmentController);
+            this.system.mapClass('fragmentInfoController', MediaPlayer.dependencies.FragmentInfoController);
             this.system.mapClass('fragmentLoader', MediaPlayer.dependencies.FragmentLoader);
             this.system.mapClass('fragmentModel', MediaPlayer.dependencies.FragmentModel);
             this.system.mapClass('manifestLoader', MediaPlayer.dependencies.ManifestLoader);
