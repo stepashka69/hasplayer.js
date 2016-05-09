95b22b05e425e2a3c4b6a7f2f67adfa7e8caa171
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-05-09 12:09:56 +0200

    build/gulp/option.js: js beautify

--- build/gulp/option.js
+++ build/gulp/option.js
@@ -4,37 +4,37 @@ var gulp = require('gulp'),
 // The actual option data.
 var data = {};
 
-var uppCase = function(obj){
+var uppCase = function(obj) {
     var newObj = {};
-    
-    for(var name in obj){
+
+    for (var name in obj) {
         newObj[name.toUpperCase()] = obj[name];
     }
-    
+
     return newObj;
 }
 
 // Get or set an option value.
 var option = gulp.option = module.exports = function(key, value) {
-  key = key.toUpperCase();
-  if (arguments.length === 2) {
-    return (data[key] = value);
-  } else {
-    return data[key];
-  }
+    key = key.toUpperCase();
+    if (arguments.length === 2) {
+        return (data[key] = value);
+    } else {
+        return data[key];
+    }
 };
 
 // Initialize option data.
 option.init = function(obj, defaultParams) {
-  data = nopt(null,null, obj, 2);
-  delete data.argv;
-  if(defaultParams){
-    data = Object.assign(data, defaultParams)
-  }
-  data = uppCase(data);
-  return data;
+    data = nopt(null, null, obj, 2);
+    delete data.argv;
+    if (defaultParams) {
+        data = Object.assign(data, defaultParams)
+    }
+    data = uppCase(data);
+    return data;
 };
 
-option.all = function(){
+option.all = function() {
     return data;
 }
