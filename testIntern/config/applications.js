3f17e8bfa964e5cb68318079217ba7cb7ebcae27
mbrechet <mbrechet.ext@orange.com>
2016-05-11 16:58:33 +0200

    [TEST_INTERN] update test to load local page

--- testIntern/config/applications.js
+++ testIntern/config/applications.js
@@ -1,7 +1,8 @@
 define({
 
     OrangeHasPlayer: {
-        development: 'http://tv-has.orange-labs.fr/orangehasplayer/dev/index.html',
+        // development: 'http://tv-has.orange-labs.fr/orangehasplayer/dev/index.html',
+        development:'http://localhost:8081/hasplayer.js/dist/samples/OrangeHasPlayerDemo/index.html',
         master: 'http://tv-has.orange-labs.fr/orangehasplayer/1.2.6/index.html'
     }
 
