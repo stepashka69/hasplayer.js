1d7eaeb1e9bf50a515687bb260870792157cf739
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-05-12 11:59:01 +0200

    testIntern/config/application.js: add local application conf

--- testIntern/config/applications.js
+++ testIntern/config/applications.js
@@ -1,9 +1,9 @@
 define({
 
     OrangeHasPlayer: {
-        // development: 'http://tv-has.orange-labs.fr/orangehasplayer/dev/index.html',
-        development:'http://localhost:8081/hasplayer.js/dist/samples/OrangeHasPlayerDemo/index.html',
-        master: 'http://tv-has.orange-labs.fr/orangehasplayer/1.2.6/index.html'
+        development: 'http://tv-has.orange-labs.fr/orangehasplayer/dev/index.html',
+        master: 'http://tv-has.orange-labs.fr/orangehasplayer/1.2.6/index.html',
+        local:'http://localhost:8081/hasplayer.js/dist/samples/OrangeHasPlayerDemo/index.html'
     }
 
 });
