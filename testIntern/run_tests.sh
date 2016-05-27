0e4be1f71af87b85341ce536b633670d295c01a9
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-05-27 15:56:18 +0200

    testIntern: delete out-of-date files

--- testIntern/run_tests.sh
+++ testIntern/run_tests.sh
@@ -3,24 +3,5 @@
 PATH=$PATH:/usr/local/bin
 npm install
 
-
-#node node_modules/intern/runner.js config=testIntern/demoPlayer/intern_common
-#results_test1=$?
-
-#node node_modules/intern/runner.js config=testIntern/demoPlayer/intern_ie
-#results_test2=$?
-
-#node node_modules/intern/runner.js config=testIntern/orangeHasPlayer/intern_common
-#results_test3=$?
-
-#node node_modules/intern/runner.js config=testIntern/orangeHasPlayer/intern_ie
-#results_test4=$?
-
-#node node_modules/intern/runner.js config=testIntern/orangeHasPlayer/intern_chrome
-#results_test5=$?
-
-#exit results_test1 && results_test2 && results_test3 && results_test4 && results_test5
-
 node node_modules/intern/runner.js config=testIntern/testsCommon.js
-
 # node node_modules/intern/runner.js config=testIntern/testsCommon.js browsers=chrome selenium=local
