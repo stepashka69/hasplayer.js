d7fe28d83e19dde5221a3b90d1fbb24ab1a233fe
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-06-06 14:31:30 +0200

    testIntern: update testing webapp links

--- testIntern/config/applications.js
+++ testIntern/config/applications.js
@@ -1,9 +1,9 @@
 define({
 
     OrangeHasPlayer: {
-        development: 'http://tv-has.orange-labs.fr/orangehasplayer/dev/index.html',
-        master: 'http://tv-has.orange-labs.fr/orangehasplayer/1.2.6/index.html',
-        local:'http://localhost:8081/hasplayer.js/dist/samples/OrangeHasPlayerDemo/index.html'
+        development: 'http://tv-has.orange-labs.fr/hasplayer_orange/dev/samples/OrangeHasPlayerDemo/index.html',
+        master: 'http://tv-has.orange-labs.fr/hasplayer_orange/1.3.1/samples/OrangeHasPlayerDemo/index.html',
+		local:'http://localhost:8081/hasplayer.js/dist/samples/OrangeHasPlayerDemo/index.html'
     }
 
 });
